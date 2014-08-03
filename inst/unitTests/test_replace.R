afbfd2c25c2c520f4188f7ec12bfb03b67cb2088
mikelove <michaelisaiahlove@gmail.com>
2014-08-03 13:04:47 +0000

    adding tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DESeq2@93150 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_replace.R
+++ inst/unitTests/test_replace.R
@@ -1,4 +1,5 @@
 test_replace <- function() {
+  # test filtering and replacement
   set.seed(1)
   dds <- makeExampleDESeqDataSet(n=100, m=12, dispMeanRel = function(x) 4/x + .5)
   counts(dds)[1,] <- rep(0L, 12)
@@ -6,10 +7,8 @@ test_replace <- function() {
   counts(dds)[3,] <- c(100000L, rep(0L, 11))
   dds0 <- DESeq(dds, minReplicatesForReplace=Inf)
   dds1 <- DESeq(dds, minReplicatesForReplace=6)
-
   head(results(dds0),3)
   head(results(dds1),3)
-
   pval0 <- results(dds0)[1:3,"pvalue"]
   pval <- results(dds1)[1:3,"pvalue"]
   LFC0 <- results(dds0)[1:3,"log2FoldChange"]
@@ -19,26 +18,31 @@ test_replace <- function() {
   checkTrue(all(is.na(pval0)))
   # not filtered
   checkTrue(all(!is.na(pval[2:3])))
-
   # counts still the same
   checkTrue(all(counts(dds1)==counts(dds)))
-  
+  # first is NA
   checkTrue(is.na(LFC[1]))
   # replaced, reduced LFC
   checkTrue(abs(LFC[2]) < abs(LFC0[2]))
   # replaced, LFC now zero
   checkTrue(LFC[3] == 0)
-  
   idx <- which(!mcols(dds1)$replace)
   # the pvalue for those not replaced is equal
   checkEquals(results(dds1)$pvalue[idx], results(dds0)$pvalue[idx])
+}
 
+test_outlier <- function() {
   # check that outlier filtering catches throughout range of mu
   set.seed(1)
-  dds <- makeExampleDESeqDataSet(n=100, m=12, interceptMean=seq(from=1,to=9,length=100), dispMeanRel=function(x) 4/x + .5)
-  idx <- rep(rep(c(TRUE,FALSE),c(1,9)),10)
-  counts(dds)[idx,1] <- 100000L
-  dds <- DESeq(dds, minReplicatesForReplace=Inf)
-  res <- results(dds)
-  checkTrue(all(is.na(res$pvalue[idx])))
+  par(mfrow=c(2,2))
+  for (m in c(10,20,60,100)) {
+    beta0 <- seq(from=1,to=16,length=100)
+    dds <- makeExampleDESeqDataSet(n=100, m=m, interceptMean=beta0, dispMeanRel=function(x) 4/x + .5)
+    idx <- rep(rep(c(TRUE,FALSE),c(1,9)),10)
+    counts(dds)[idx,1] <- as.integer(1000 * 2^beta0[idx])
+    dds <- DESeq(dds, minReplicatesForReplace=Inf)
+    # plot(assays(dds)[["cooks"]][,1], col=idx + 1, log="y", main=m);abline(h=qf(.99,2,m-2))
+    res <- results(dds)
+    checkTrue(all(is.na(res$pvalue[idx])))
+  }
 }
