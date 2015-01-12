a2691f96c3b373d6f2d2ef6460b71324857f8831
mikelove <michaelisaiahlove@gmail.com>
2015-01-12 17:25:31 +0000

    updating galaxy script
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DESeq2@98273 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/script/deseq2.R
+++ inst/script/deseq2.R
@@ -10,7 +10,12 @@
 #
 # the first variable in 'factors' and first column in 'sample_table' will be the primary factor.
 # the levels of the primary factor are used in the order of appearance in factors or in sample_table.
-# e.g., factors in order A,B,C produces comparisons C vs A, B vs A, C vs B
+#
+# by default, levels in the order A,B,C produces a single comparison of B vs A, to a single file 'outfile'
+#
+# for the 'many_contrasts' flag, levels in the order A,B,C produces comparisons C vs A, B vs A, C vs B,
+# to a number of files using the 'outfile' prefix: 'outfile.condition_C_vs_A' etc.
+# all plots will still be sent to a single PDF, named by the arg 'plots', with extra pages.
 #
 # fit_type is an integer valued argument, with the options from ?estimateDisperions
 #   1 "parametric"
@@ -30,13 +35,14 @@ args <- commandArgs(trailingOnly = TRUE)
 # get options, using the spec as defined by the enclosed list.
 # we read the options from the default: commandArgs(TRUE).
 spec <- matrix(c(
-  "verbose", "v", 0, "logical",
+  "quiet", "q", 0, "logical",
   "help", "h", 0, "logical",
-  "outfile_prefix", "o", 1, "character",
+  "outfile", "o", 1, "character",
   "factors", "f", 1, "character",
-  "sample_table", "s", 1, "character",
   "plots" , "p", 1, "character",
+  "sample_table", "s", 1, "character",
   "fit_type", "t", 1, "integer",
+  "many_contrasts", "m", 0, "logical",
   "outlier_replace_off" , "a", 0, "logical",
   "outlier_filter_off" , "b", 0, "logical",
   "auto_mean_filter_off", "c", 0, "logical",
@@ -52,18 +58,19 @@ if (!is.null(opt$help)) {
 }
 
 # enforce the following required arguments
-if (is.null(opt$outfile_prefix)) {
-  print("'outfile_prefix' is required")
+if (is.null(opt$outfile)) {
+  cat("'outfile' is required\n")
   q(status=1)
 }
 if (is.null(opt$sample_table) & is.null(opt$factors)) {
-  print("'factors' or 'sample_table' is required")
+  cat("'factors' or 'sample_table' is required\n")
   q(status=1)
 }
-verbose <- if (is.null(opt$verbose)) {
-  FALSE
-} else {
+
+verbose <- if (is.null(opt$quiet)) {
   TRUE
+} else {
+  FALSE
 }
 
 suppressPackageStartupMessages({
@@ -113,6 +120,15 @@ if (!is.null(opt$factors)) {
 primaryFactor <- factors[1]
 designFormula <- as.formula(paste("~", paste(rev(factors), collapse=" + ")))
 
+if (verbose) {
+  cat("DESeq2 run information\n\n")
+  cat("sample table:\n")
+  print(sampleTable[,-c(1:2),drop=FALSE])
+  cat("\ndesign formula:\n")
+  print(designFormula)
+  cat("\n\n")
+}
+
 # these are plots which are made once for each analysis
 generateGenericPlots <- function(dds, factors) {
   rld <- rlog(dds)
@@ -126,24 +142,35 @@ generateGenericPlots <- function(dds, factors) {
   plotDispEsts(dds, main="Dispersion estimates")
 }
 
-# these are plots which are made for each comparison, e.g.
+# these are plots which can be made for each comparison, e.g.
 # once for C vs A and once for B vs A
 generateSpecificPlots <- function(res, threshold, title_suffix) {
   use <- res$baseMean > threshold
-  h1 <- hist(res$pvalue[!use], breaks=50, plot=FALSE)
-  h2 <- hist(res$pvalue[use], breaks=50, plot=FALSE)
-  colori <- c("filtered (low count)"="khaki", "not filtered"="powderblue")
-  barplot(height = rbind(h1$counts, h2$counts), beside = FALSE,
-          col = colori, space = 0, xlab="p-values", ylab="frequency",
-          main=paste("Histogram of p-values for",title_suffix))
-  text(x = c(0, length(h1$counts)), y = 0, label=paste(c(0,1)), adj=c(0.5,1.7), xpd=NA)
-  legend("topright", fill=rev(colori), legend=rev(names(colori)))
-  plotMA(res, main= paste("MA-plot for",title_suffix), ylim=range(res$log2FoldChange, na.rm=TRUE))
+  if (sum(!use) == 0) {
+    h <- hist(res$pvalue, breaks=0:50/50, plot=FALSE)
+    barplot(height = h$counts,
+            col = "powderblue", space = 0, xlab="p-values", ylab="frequency",
+            main=paste("Histogram of p-values for",title_suffix))
+    text(x = c(0, length(h1$counts)), y = 0, label=paste(c(0,1)), adj=c(0.5,1.7), xpd=NA)
+  } else {
+    h1 <- hist(res$pvalue[!use], breaks=0:50/50, plot=FALSE)
+    h2 <- hist(res$pvalue[use], breaks=0:50/50, plot=FALSE)
+    colori <- c("filtered (low count)"="khaki", "not filtered"="powderblue")
+    barplot(height = rbind(h1$counts, h2$counts), beside = FALSE,
+            col = colori, space = 0, xlab="p-values", ylab="frequency",
+            main=paste("Histogram of p-values for",title_suffix))
+    text(x = c(0, length(h1$counts)), y = 0, label=paste(c(0,1)), adj=c(0.5,1.7), xpd=NA)
+    legend("topright", fill=rev(colori), legend=rev(names(colori)), bg="white")
+  }
+    plotMA(res, main= paste("MA-plot for",title_suffix), ylim=range(res$log2FoldChange, na.rm=TRUE))
 }
 
-if (verbose) message(paste("primary factor:",primaryFactor))
-if (length(factors) > 1) {
-  if (verbose) message(paste("other factors in design:",paste(factors[-length(factors)],collapse=",")))
+if (verbose) {
+  cat(paste("primary factor:",primaryFactor,"\n"))
+  if (length(factors) > 1) {
+    cat(paste("other factors in design:",paste(factors[-length(factors)],collapse=","),"\n"))
+  }
+  cat("\n---------------------\n")
 }
 
 # if JSON input from Galaxy, path is absolute
@@ -159,20 +186,20 @@ dds <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
                                   directory = dir,
                                   design =  designFormula)
 
-if (verbose) message(paste(ncol(dds), "samples with counts over", nrow(dds), "genes"))
+if (verbose) cat(paste(ncol(dds), "samples with counts over", nrow(dds), "genes\n"))
 
 # optional outlier behavior
 if (is.null(opt$outlier_replace_off)) {
   minRep <- 7
 } else {
   minRep <- Inf
-  if (verbose) message("outlier replacement off")
+  if (verbose) cat("outlier replacement off\n")
 }
 if (is.null(opt$outlier_filter_off)) {
   cooksCutoff <- TRUE
 } else {  
   cooksCutoff <- FALSE
-  if (verbose) message("outlier filtering off")
+  if (verbose) cat("outlier filtering off\n")
 }
 
 # optional automatic mean filtering
@@ -180,27 +207,32 @@ if (is.null(opt$auto_mean_filter_off)) {
   independentFiltering <- TRUE
 } else {
   independentFiltering <- FALSE
-  if (verbose) message("automatic filtering on the mean off")
+  if (verbose) cat("automatic filtering on the mean off\n")
 }
 
+# shrinkage of LFCs
 if (is.null(opt$beta_prior_off)) {
   betaPrior <- TRUE
 } else {
   betaPrior <- FALSE
+  if (verbose) cat("beta prior off\n")
 }
 
+# dispersion fit type
 if (is.null(opt$fit_type)) {
   fitType <- "parametric"
 } else {
   fitType <- c("parametric","local","mean")[opt$fit_type]
 }
 
+if (verbose) cat(paste("using disperion fit type:",fitType,"\n"))
+
 # run the analysis
-dds <- DESeq(dds, fitType=fitType, betaPrior=betaPrior, minReplicatesForReplace=minRep, quiet=!verbose)
+dds <- DESeq(dds, fitType=fitType, betaPrior=betaPrior, minReplicatesForReplace=minRep)
 
 # create the generic plots and leave the device open
 if (!is.null(opt$plots)) {
-  if (verbose) message("creating plots")
+  if (verbose) cat("creating plots\n")
   pdf(opt$plots)
   generateGenericPlots(dds, factors)
 }
@@ -208,42 +240,72 @@ if (!is.null(opt$plots)) {
 n <- nlevels(colData(dds)[[primaryFactor]])
 allLevels <- levels(colData(dds)[[primaryFactor]])
 
-# rotate through the possible contrasts of the primary factor
-# write out a sorted table of results with the contrast as a suffix
-# add contrast specific plots to the device
-for (i in seq_len(n-1)) {
-  ref <- allLevels[i]
-  contrastLevels <- allLevels[(i+1):n]
-  for (lvl in contrastLevels) {
-    res <- results(dds, contrast=c(primaryFactor, lvl, ref),
-                   cooksCutoff=cooksCutoff,
-                   independentFiltering=independentFiltering)
-    resSorted <- res[order(res$padj),]
-    outDF <- as.data.frame(resSorted)
-    outDF$geneID <- rownames(outDF)
-    outDF <- outDF[,c("geneID", "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")]
-    filename <- paste0(opt$outfile_prefix,".",primaryFactor,"_",lvl,"_vs_",ref)
-    write.table(outDF, file=filename, sep="\t", quote=FALSE)
-    if (independentFiltering) {
-      threshold <- unname(attr(res, "filterThreshold"))
-    } else {
-      threshold <- 0
-    }
-    title_suffix <- paste0(primaryFactor,": ",lvl," vs ",ref)
-    if (!is.null(opt$plots)) {
-      generateSpecificPlots(res, threshold, title_suffix)
+if (is.null(opt$many_contrasts)) {
+  # only contrast the first and second level of the primary factor
+  ref <- allLevels[1]
+  lvl <- allLevels[2]
+  res <- results(dds, contrast=c(primaryFactor, lvl, ref),
+                 cooksCutoff=cooksCutoff,
+                 independentFiltering=independentFiltering)
+  if (verbose) {
+    cat("summary of results\n")
+    cat(paste0(primaryFactor,": ",lvl," vs ",ref,"\n"))
+    print(summary(res))
+  }
+  resSorted <- res[order(res$padj),]
+  outDF <- as.data.frame(resSorted)
+  outDF$geneID <- rownames(outDF)
+  outDF <- outDF[,c("geneID", "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")]
+  filename <- opt$outfile
+  write(paste0("# DESeq2 results table. ",primaryFactor,": ",lvl," vs ",ref), file=filename)
+  # suppress the warning that we are appending column names to a file
+  suppressWarnings({ write.table(outDF, file=filename, sep="\t", quote=FALSE, append=TRUE) })
+  if (independentFiltering) {
+    threshold <- unname(attr(res, "filterThreshold"))
+  } else {
+    threshold <- 0
+  }
+  title_suffix <- paste0(primaryFactor,": ",lvl," vs ",ref)
+  if (!is.null(opt$plots)) {
+    generateSpecificPlots(res, threshold, title_suffix)
+  }
+} else {
+  # rotate through the possible contrasts of the primary factor
+  # write out a sorted table of results with the contrast as a suffix
+  # add contrast specific plots to the device
+  for (i in seq_len(n-1)) {
+    ref <- allLevels[i]
+    contrastLevels <- allLevels[(i+1):n]
+    for (lvl in contrastLevels) {
+      res <- results(dds, contrast=c(primaryFactor, lvl, ref),
+                     cooksCutoff=cooksCutoff,
+                     independentFiltering=independentFiltering)
+      resSorted <- res[order(res$padj),]
+      outDF <- as.data.frame(resSorted)
+      outDF$geneID <- rownames(outDF)
+      outDF <- outDF[,c("geneID", "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")]
+      filename <- paste0(opt$outfile,".",primaryFactor,"_",lvl,"_vs_",ref)
+      write.table(outDF, file=filename, sep="\t", quote=FALSE)
+      if (independentFiltering) {
+        threshold <- unname(attr(res, "filterThreshold"))
+      } else {
+        threshold <- 0
+      }
+      title_suffix <- paste0(primaryFactor,": ",lvl," vs ",ref)
+      if (!is.null(opt$plots)) {
+        generateSpecificPlots(res, threshold, title_suffix)
+      }
     }
   }
 }
 
-# write out the sample table for record keeping
-# write.table(sampleTable, file=paste0(opt$outfile_prefix,".sampleTable"), sep="\t", quote=FALSE)
-write.table(sampleTable, file=opt$outfile_prefix, sep="\t", quote=FALSE)
-
 # close the plot device
 if (!is.null(opt$plots)) {
+  cat("closing plot device\n")
   dev.off()
 }
+
+cat("Session information:\n\n")
 
 sessionInfo()
 
