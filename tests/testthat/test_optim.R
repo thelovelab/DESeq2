cd4cc9e6a7a22821df32f14acf4be5d39afe1acb
mikelove <michaelisaiahlove@gmail.com>
2015-10-03 20:58:21 +0000

    adding test for lfcSE
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DESeq2@109160 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_optim.R
+++ tests/testthat/test_optim.R
@@ -21,3 +21,14 @@ fitOptim <- DESeq2:::fitNbinomGLMs(dds, modelMatrix=modelMatrix,
                                    useQR=TRUE, forceOptim=TRUE)
 expect_equal(fit$betaMatrix, fitOptim$betaMatrix,tolerance=1e-6)
 expect_equal(fit$betaSE, fitOptim$betaSE,tolerance=1e-6)
+
+# test optim gives same lfcSE
+set.seed(1)
+dds <- makeExampleDESeqDataSet(n=100, m=10)
+counts(dds)[1,] <- c(rep(0L,5),c(1000L,1000L,0L,0L,0L))
+dds <- DESeq(dds, betaPrior=FALSE)
+res1 <- results(dds, contrast=c("condition","B","A"))
+res2 <- results(dds, contrast=c(0,1))
+expect_true(all.equal(res1$lfcSE, res2$lfcSE))
+expect_true(all.equal(res1$pvalue, res2$pvalue))
+
