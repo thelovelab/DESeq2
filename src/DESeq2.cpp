16f90e40d870a1f17c0e02e141c94d911c17979a
const-ae <artjom31415@googlemail.com>
2019-07-30 09:43:31 +0200

    Fix bug in fast calculation of hat matrix diagonal
    Move the initialization of the hat_matrix_diag vector right in front of
    the for loop, so that it isn't accumulating the values from previous
    iterations.

--- src/DESeq2.cpp
+++ src/DESeq2.cpp
@@ -256,7 +256,6 @@ List fitBeta(SEXP ySEXP, SEXP xSEXP, SEXP nfSEXP, SEXP alpha_hatSEXP, SEXP contr
   arma::mat beta_var_mat = arma::zeros(beta_mat.n_rows, beta_mat.n_cols);
   arma::mat contrast_num = arma::zeros(beta_mat.n_rows, 1);
   arma::mat contrast_denom = arma::zeros(beta_mat.n_rows, 1);
-  arma::vec hat_matrix_diag = arma::zeros(x.n_rows);
   arma::mat hat_diagonals = arma::zeros(y.n_rows, y.n_cols);
   arma::colvec lambda = as<arma::colvec>(lambdaSEXP);
   arma::colvec contrast = as<arma::colvec>(contrastSEXP);
@@ -398,6 +397,7 @@ List fitBeta(SEXP ySEXP, SEXP xSEXP, SEXP nfSEXP, SEXP alpha_hatSEXP, SEXP contr
       w_vec = mu_hat/(1.0 + alpha_hat(i) * mu_hat);
       w_sqrt_vec = sqrt(w_vec);
     }
+    arma::vec hat_matrix_diag = arma::zeros(x.n_rows);
     arma::mat xw = x.each_col() % w_sqrt_vec;
     arma::mat xtwxr_inv = (x.t() * (x.each_col() % w_vec) + ridge).i();
     
