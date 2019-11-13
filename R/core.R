5636e4b461013cfdff462030cec4d33d6917a9fd
mikelove <michaelisaiahlove@gmail.com>
2019-11-13 09:56:05 -0500

    removing pkg dependencies

--- R/core.R
+++ R/core.R
@@ -209,10 +209,8 @@ NULL
 #'
 #' @importFrom locfit locfit
 #' @importFrom genefilter rowVars filtered_p
-#' @importFrom Hmisc wtd.quantile
-#' 
 #' @importFrom graphics axis hist plot points
-#' @importFrom stats Gamma as.formula coefficients df dnbinom dnorm formula glm loess lowess model.matrix optim p.adjust pchisq pnorm prcomp predict pt qf qnorm rchisq relevel rnbinom rnorm runif splinefun terms terms.formula
+#' @importFrom stats Gamma as.formula coefficients df dnbinom dnorm formula glm loess lowess model.matrix optim p.adjust pchisq pnorm prcomp predict pt qf qnorm rchisq relevel rnbinom rnorm runif splinefun terms terms.formula approx
 #' @importFrom utils read.table
 #' @importFrom stats4 summary
 #' 
@@ -1064,8 +1062,9 @@ estimateDispersionsPriorVar <- function(object, minDisp=1e-8, modelMatrix=NULL) 
 #' of the observed MLE coefficients to a zero-centered Normal distribution.
 #' In a change of methods since the 2014 paper,
 #' the weighted upper quantile is calculated using the
-#' \code{wtd.quantile} function from the Hmisc package. The weights are
-#' the inverse of the expected variance of log counts, so the inverse of
+#' \code{wtd.quantile} function from the Hmisc package
+#' (function has been copied into DESeq2 to avoid extra dependencies).
+#' The weights are the inverse of the expected variance of log counts, so the inverse of
 #' \eqn{1/\bar{\mu} + \alpha_{tr}}{1/mu-bar + alpha_tr} using the mean of
 #' normalized counts and the trended dispersion fit. The weighting ensures
 #' that noisy estimates of log fold changes from small count genes do not
@@ -2154,7 +2153,7 @@ matchUpperQuantileForVariance <- function(x, upperQuantile=.05) {
 }
 
 matchWeightedUpperQuantileForVariance <- function(x, weights, upperQuantile=.05) {
-  sdEst <- wtd.quantile(abs(x), weights=weights, 1 - upperQuantile, normwt=TRUE) / qnorm(1 - upperQuantile/2)
+  sdEst <- Hmisc.wtd.quantile(abs(x), weights=weights, 1 - upperQuantile, normwt=TRUE) / qnorm(1 - upperQuantile/2)
   unname(sdEst)^2
 }
 
@@ -2479,3 +2478,169 @@ getAndCheckWeights <- function(object, modelMatrix) {
   }
   list(object=object,weights=weights,useWeights=useWeights)
 }
+
+#################################################
+## functions from Hmisc for Hmisc.wtd.quantile ##
+#################################################
+
+# this and the following two functions are copied from Hmisc
+# to avoid extra package dependencies in DESeq2 (same license as Hmisc),
+# with the alteration of commenting out `isdate` test
+# https://cran.r-project.org/package=Hmisc
+Hmisc.wtd.quantile <- function(x, weights=NULL, probs=c(0, .25, .5, .75, 1), 
+                         type=c('quantile','(i-1)/(n-1)','i/(n+1)','i/n'), 
+                         normwt=FALSE, na.rm=TRUE)
+{
+  if(! length(weights))
+    return(quantile(x, probs=probs, na.rm=na.rm))
+
+  type <- match.arg(type)
+  if(any(probs < 0 | probs > 1))
+    stop("Probabilities must be between 0 and 1 inclusive")
+
+  nams <- paste(format(round(probs * 100, if(length(probs) > 1) 
+                             2 - log10(diff(range(probs))) else 2)), 
+                "%", sep = "")
+
+  i <- is.na(weights) | weights == 0
+  if(any(i)) {
+    x <- x[! i]
+    weights <- weights[! i]
+    }
+  if(type == 'quantile') {
+    w <- Hmisc.wtd.table(x, weights, na.rm=na.rm, normwt=normwt, type='list')
+    x     <- w$x
+    wts   <- w$sum.of.weights
+    n     <- sum(wts)
+    order <- 1 + (n - 1) * probs
+    low   <- pmax(floor(order), 1)
+    high  <- pmin(low + 1, n)
+    order <- order %% 1
+    ## Find low and high order statistics
+    ## These are minimum values of x such that the cum. freqs >= c(low,high)
+    allq <- approx(cumsum(wts), x, xout=c(low,high), 
+                   method='constant', f=1, rule=2)$y
+    k <- length(probs)
+    quantiles <- (1 - order)*allq[1:k] + order*allq[-(1:k)]
+    names(quantiles) <- nams
+    return(quantiles)
+  } 
+  w <- Hmisc.wtd.Ecdf(x, weights, na.rm=na.rm, type=type, normwt=normwt)
+  structure(approx(w$ecdf, w$x, xout=probs, rule=2)$y, 
+            names=nams)
+}
+
+
+Hmisc.wtd.Ecdf <- function(x, weights=NULL, 
+                     type=c('i/n','(i-1)/(n-1)','i/(n+1)'), 
+                     normwt=FALSE, na.rm=TRUE)
+{
+  type <- match.arg(type)
+  switch(type,
+         '(i-1)/(n-1)'={a <- b <- -1},
+         'i/(n+1)'    ={a <- 0; b <- 1},
+         'i/n'        ={a <- b <- 0})
+
+  if(! length(weights)) {
+    ##.Options$digits <- 7  ## to get good resolution for names(table(x))
+    oldopt <- options('digits')
+    options(digits=7)
+    on.exit(options(oldopt))
+    cumu <- table(x)    ## R does not give names for cumsum
+    #isdate <- testDateTime(x)  ## 31aug02
+    ax <- attributes(x)
+    ax$names <- NULL
+    x <- as.numeric(names(cumu))
+    #if(isdate) attributes(x) <- c(attributes(x),ax)
+    cumu <- cumsum(cumu)
+    cdf <- (cumu + a)/(cumu[length(cumu)] + b)
+    if(cdf[1]>0) {
+      x <- c(x[1], x);
+      cdf <- c(0,cdf)
+    }
+
+    return(list(x = x, ecdf=cdf))
+  }
+
+  w <- Hmisc.wtd.table(x, weights, normwt=normwt, na.rm=na.rm)
+  cumu <- cumsum(w$sum.of.weights)
+  cdf <- (cumu + a)/(cumu[length(cumu)] + b)
+  list(x = c(if(cdf[1]>0) w$x[1], w$x), ecdf=c(if(cdf[1]>0)0, cdf))
+}
+
+
+Hmisc.wtd.table <- function(x, weights=NULL, type=c('list','table'), 
+                      normwt=FALSE, na.rm=TRUE)
+{
+  type <- match.arg(type)
+  if(! length(weights))
+    weights <- rep(1, length(x))
+
+  #isdate <- testDateTime(x)  ## 31aug02 + next 2
+  ax <- attributes(x)
+  ax$names <- NULL
+  
+  if(is.character(x)) x <- as.factor(x)
+  lev <- levels(x)
+  x <- unclass(x)
+  
+  if(na.rm) {
+    s <- ! is.na(x + weights)
+    x <- x[s, drop=FALSE]    ## drop is for factor class
+    weights <- weights[s]
+  }
+
+  n <- length(x)
+  if(normwt)
+    weights <- weights * length(x) / sum(weights)
+
+  i <- order(x)  # R does not preserve levels here
+  x <- x[i]; weights <- weights[i]
+
+  if(anyDuplicated(x)) {  ## diff(x) == 0 faster but doesn't handle Inf
+    weights <- tapply(weights, x, sum)
+    if(length(lev)) {
+      levused <- lev[sort(unique(x))]
+      if((length(weights) > length(levused)) &&
+         any(is.na(weights)))
+        weights <- weights[! is.na(weights)]
+
+      if(length(weights) != length(levused))
+        stop('program logic error')
+
+      names(weights) <- levused
+    }
+
+    if(! length(names(weights)))
+      stop('program logic error')
+
+    if(type=='table')
+      return(weights)
+
+    # modified from Hmisc::all.is.numeric
+    x <- as.numeric(names(weights))
+    #if(isdate)
+    #  attributes(x) <- c(attributes(x),ax)
+
+    names(weights) <- NULL
+    return(list(x=x, sum.of.weights=weights))
+  }
+
+  xx <- x
+  #if(isdate)
+  #  attributes(xx) <- c(attributes(xx),ax)
+
+  if(type=='list')
+    list(x=if(length(lev))lev[x]
+           else xx, 
+         sum.of.weights=weights)
+  else {
+    names(weights) <- if(length(lev)) lev[x]
+                      else xx
+    weights
+  }
+}
+
+##############################
+## end functions from Hmisc ##
+##############################
