e1030e50a6997da3a49b27f913520b1dc28085e6
mikelove <michaelisaiahlove@gmail.com>
2020-08-22 20:18:29 -0400

    add back tests, seems fine on my end

--- tests/testthat/test_LRT.R
+++ tests/testthat/test_LRT.R
@@ -14,12 +14,12 @@ test_that("test='LRT' gives correct errors", {
 
 test_that("test='LRT' with quasi-likelihood estimates gives correct errors", {
   
-  ## dds <- makeExampleDESeqDataSet(n=100, m=4)
-  ## dds$group <- factor(c(1,2,1,2))
-  ## design(dds) <- ~ condition + group
-  ## expect_warning(DESeq(dds, test="Wald", fitType = "glmGamPoi"))
-  ## dds <- estimateSizeFactors(dds)
-  ## dds_gp <- estimateDispersions(dds)
-  ## expect_error(nbinomLRT(dds_gp, reduced = ~ condition, type = "glmGamPoi"))
+  dds <- makeExampleDESeqDataSet(n=100, m=4)
+  dds$group <- factor(c(1,2,1,2))
+  design(dds) <- ~ condition + group
+  expect_warning(DESeq(dds, test="Wald", fitType = "glmGamPoi"))
+  dds <- estimateSizeFactors(dds)
+  dds_gp <- estimateDispersions(dds)
+  expect_error(nbinomLRT(dds_gp, reduced = ~ condition, type = "glmGamPoi"))
     
 })
