15f2ec90e8707705d43c1cd66d32b7e99fd6a741
Mike Love <mikelove@users.noreply.github.com>
2026-03-12 13:58:58 -0400

    dont use threshold in looking at LFC sign, fixes #127

--- R/methods.R
+++ R/methods.R
@@ -806,8 +806,8 @@ summary.DESeqResults <- function(object, alpha, ...) {
   }
   cat("\n")
   notallzero <- sum(object$baseMean > 0)
-  up <- sum(object[[test.col]] < alpha & object$log2FoldChange > T, na.rm=TRUE)
-  down <- sum(object[[test.col]] < alpha & object$log2FoldChange < -T, na.rm=TRUE)
+  up <- sum(object[[test.col]] < alpha & object$log2FoldChange > 0, na.rm=TRUE)
+  down <- sum(object[[test.col]] < alpha & object$log2FoldChange < 0, na.rm=TRUE)
   if (!sval) {
     filt <- sum(!is.na(object$pvalue) & is.na(object$padj))
     outlier <- sum(object$baseMean > 0 & is.na(object$pvalue))
